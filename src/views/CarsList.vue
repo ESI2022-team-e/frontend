0e090110edd21b09da3960c522404a4113f02127
Karoliine Holter <44437975+karoliineh@users.noreply.github.com>
2022-05-14 11:40:52 +0300

    Invoices initial (#3)
    * Bugfix
    
    * Add sass and move all styling to assets/styles
    
    * Move navbar content from app to components
    
    * Rename NavBar
    
    * Add initial invoice page - manager can see all invoices
    
    * Change bootstrap dependency

--- src/views/CarsList.vue
+++ src/views/CarsList.vue
@@ -3,7 +3,7 @@
     <section class="main-container">
       <h1>Car Catalogue </h1>
       <div class="car-profile" v-for="car in cars" :key="car.id">
-        <img alt="car img" src="../assets/images.jpg">
+        <img alt="car img" src="src/assets/img/images.jpg">
         <h2>Car {{ car.mark }} <br>
           {{ car.model }}</h2>
         <div class="text">
@@ -29,55 +29,3 @@ export default {
   }
 }
 </script>
-
-<style>
-h1 {
-  font-size: 45px;
-  text-align: center;
-  text-transform: uppercase;
-  margin: 20px;
-}
-
-.car-profile {
-  width: 100%;
-  display: inline-block;
-  border: 1px solid #111111;
-  border-radius: 5px;
-  text-align: center;
-  margin: 10px;
-}
-
-.car-profile img {
-  width: 40%;
-  height: 40%;
-  float: left;
-  margin: 20px;
-}
-
-.car-profile h2 {
-  font-size: 34px;
-  margin: 20px;
-}
-
-.car-profile p {
-  text-align: left;
-  margin-left: 35px;
-}
-
-.car-profile .text {
-  display: flex;
-  justify-content: space-between;
-}
-
-.select-button {
-  background-color: white;
-  height: 55px;
-  width: 190px;
-  line-height: 10px;
-  font-weight: bold;
-  text-transform: uppercase;
-  border: 5px solid #34d484;
-  margin: 15px 35px 15px 15px;
-}
-
-</style>
